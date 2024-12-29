087e5c63816abedce4fcce63e3268f21b50b78aa
dugless11 <112522575+dugless11@users.noreply.github.com>
2024-12-28 17:23:13 -0700

    sprite fix and added test body

---
+++ TSAGameDev24/tali.gd
@@ -0,0 +1,39 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+var DASH_ENABLED = true
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	#if not is_on_floor():
+	#	velocity += get_gravity() * delta
+
+	# Handle jump.
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	#	velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var xdirection = Input.get_axis("ui_left", "ui_right")
+	if xdirection:
+		velocity.x = xdirection * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	var ydirection = Input.get_axis("ui_up", "ui_down")
+	if ydirection:
+		velocity.y = ydirection * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+	move_and_slide()
+
+
+func _on_dash_cooldown_timeout():
+	pass # Replace with function body.
+
+
+func _on_dash_quicktime_timeout():
+	pass # Replace with function body.
